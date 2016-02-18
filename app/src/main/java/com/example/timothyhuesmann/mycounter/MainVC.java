57b676a2fcbba338132f5847e56277dbb5e15199
Timothy Huesmann <timothy.huesmann@gmail.com>
2016-02-18 15:27:38 -0600

    Started adding first interface

--- app/src/main/java/com/example/timothyhuesmann/mycounter/MainVC.java
+++ app/src/main/java/com/example/timothyhuesmann/mycounter/MainVC.java
@@ -1,29 +1,42 @@
 package com.example.timothyhuesmann.mycounter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
 
 
-public class MainVC extends ActionBarActivity {
+public class MainVC extends ActionBarActivity
+{
+
+    Button enterButton_MainVC;
+    EditText usernameTF_MainVC;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_vc);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main_vc, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -35,5 +48,38 @@ public class MainVC extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addListenerOnButton()
+    {
+        enterButton_MainVC = (Button) findViewById(R.id.enterButton_MainVC);
+        usernameTF_MainVC = (EditText) findViewById(R.id.usernameTF_MainVC);
+        enterButton_MainVC.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(usernameTF_MainVC.getText() != null)
+                {
+
+                }
+                else
+                {
+                    new AlertDialog.Builder(MainVC.this)
+                        .setMessage("Error: Username not Entered")
+                        .setNeutralButton("OK", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+
+                            }
+                        })
+                        .show();
+                }
+            }
+
+
+        });
     }
 }
