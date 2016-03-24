83eabe5546d622c237b5166f1b5d3ca5e09e7039
Timothy Huesmann <timothy.huesmann@gmail.com>
2016-03-24 18:23:32 -0500

    Added the HTTP Get for logging in.

--- app/src/main/java/com/example/timothyhuesmann/mycounter/MainVC.java
+++ app/src/main/java/com/example/timothyhuesmann/mycounter/MainVC.java
@@ -2,14 +2,33 @@ package com.example.timothyhuesmann.mycounter;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.preference.DialogPreference;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
+import android.widget.Spinner;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+
+import com.firebase.client.Firebase;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
 
 
 public class MainVC extends ActionBarActivity
@@ -17,12 +36,19 @@ public class MainVC extends ActionBarActivity
 
     Button enterButton_MainVC;
     EditText usernameTF_MainVC;
+    Spinner regionSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_vc);
+        Firebase.setAndroidContext(this);
+        regionSpinner = (Spinner) findViewById(R.id.regionSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.regions, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        regionSpinner.setAdapter(adapter);
+        addListenerOnButton();
     }
 
 
@@ -54,32 +80,54 @@ public class MainVC extends ActionBarActivity
     {
         enterButton_MainVC = (Button) findViewById(R.id.enterButton_MainVC);
         usernameTF_MainVC = (EditText) findViewById(R.id.usernameTF_MainVC);
+
         enterButton_MainVC.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                if(usernameTF_MainVC.getText() != null)
+                if (usernameTF_MainVC.getText().toString().isEmpty())
                 {
+                    new AlertDialog.Builder(MainVC.this)
+                            .setMessage("Error: Username not Entered")
+                            .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
 
+                                }
+                            })
+                            .show();
                 }
                 else
                 {
-                    new AlertDialog.Builder(MainVC.this)
-                        .setMessage("Error: Username not Entered")
-                        .setNeutralButton("OK", new DialogInterface.OnClickListener()
-                        {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which)
-                            {
+                    String dataUrl = "https://mycounter-app.herokuapp.com/login/" + usernameTF_MainVC.getText().toString() + '/' + regionSpinner.getSelectedItem().toString();
+                    // Instantiate the RequestQueue.
+                            RequestQueue queue = Volley.newRequestQueue(MainVC.this);
 
-                            }
-                        })
-                        .show();
+
+// Request a string response from the provided URL.
+                    StringRequest stringRequest = new StringRequest(Request.Method.GET, dataUrl,
+                            new Response.Listener<String>()
+                            {
+                                @Override
+                                public void onResponse(String response)
+                                {
+                                    System.out.println(response);
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error)
+                        {
+                           System.out.println("Error:"+ error);
+                        }
+                    });
+// Add the request to the RequestQueue.
+                    queue.add(stringRequest);
                 }
             }
-
-
         });
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/timothyhuesmann/mycounter/MyCounterCore.java
@@ -0,0 +1,21 @@
+package com.example.timothyhuesmann.mycounter;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by timothyhuesmann on 2/25/16.
+ */
+
+public class MyCounterCore
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+
+}
